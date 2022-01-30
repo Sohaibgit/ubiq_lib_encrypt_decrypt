0b9026be4bbacfab8d7283d0cb879b76290d9958
Sohaib Khan <sohaib5764@gmail.com>
2022-01-30 16:30:45 +0500

    Ubiq Library Demo

---
+++ Ubiq-Demo/src/main/java/com/dev/ubiq/Credentials.java
@@ -0,0 +1,7 @@
+package com.dev.ubiq;
+
+public class Credentials {
+    public static final String ACCESS_KEY_ID = "M7RSgRf9yc8ECl4ysqaimZfj";
+    public static final String SECRET_SIGNING_KEY = "ao8GY0igv3rnEoX3dSrcqtMvWl8SHc4I0VferxCwzg4w";
+    public static final String SECRET_CRYPTO_ACCESS_KEY = "6diWmtz6Sep3LBxLGv/+FlgP59uaWiXMST/GEvezkRJJ";
+}
